89aed293859f6a9ed8a8f8d3e4ca24e930fa5f36
BotnightTown <minecraftv871@gmail.com>
2024-06-13 20:18:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var offcanvasExample = document.getElementById('offcanvasExample');
+//     var navbarContent = document.querySelector('.navbar-collapse').innerHTML;
+
+//     offcanvasExample.addEventListener('show.bs.offcanvas', function() {
+//       document.getElementById('offcanvasContent').innerHTML = navbarContent;
+//     });
+//   });
